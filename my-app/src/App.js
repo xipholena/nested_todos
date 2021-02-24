2a30291e43454cd95f0fadabe0732cc8a3a6dc8c
Your Name <mogolgogol.90@gmail.com>
2021-02-24 22:09:57 +0200

    24.02

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,34 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+
 import './App.css';
-//import {Todo} from './components/Todo'
+import {Todo} from './components/Todo'
 
  const init = [
    {id: 1, title: 'work hard', index: 1,},
    {id: 2, title: 'play hard', index: 2},
    {id: 3, title: 'resolve', index: 3,},
-   {id: 4, title: 'reject', index: 4,},
+   {id: 4, title: 'reject', index: 4, 
+   sublist: [
+     {id: 100, title: 'sublist todo', index: 100, 
+      sublist: [
+        {id: 200, title: 'sublist todo', index: 200}
+      ]},
+    ]},
  ]
+
 
 function App() {
   const [todos, setTodos] = useState(init);
   const [todoTitle, setTodoTitle] = useState('');
-  console.log(todos);
 
   const changeHandler = ({target}) => {
     setTodoTitle(target.value)
   }
   
-  const addTodoHandler = ({target}) => {
+  const addTodoHandler = () => {
     const newTodo = {
       id: +(new Date()),
-      title: target.previousElementSibling.value,
+      title: todoTitle, 
       index: todos.length + 1,
     }
     setTodos(currentState => ([
@@ -31,83 +38,90 @@ function App() {
     setTodoTitle('');
   }
 
-  const removeListHandler = ({target}) => {
-    const targetTodoId = +target.closest('.todo-list__item').id;
-    setTodos(todos.filter(item=> item.id !== targetTodoId));
+  const removeListHandler = (id) => {
+    setTodos(todos.filter(item=> item.id !== id));
   }
 
   const handleSort = () => {
-    setTodos(todos.sort((a, b) => a.index - b.index))
+    setTodos(todos=> todos.sort((a, b) => a.index - b.index))
   }
 
-  const increaseIndex = ({target}) => {
-    const targetTodoId = +target.closest('.todo-list__item').id;
+  const increaseIndex = (id) => {
     
-    setTodos(() => {
-     //todos.find(item=> item.id === targetTodoId).index -= 1
-     // todos.find(item=> item.id === targetTodoId-1).index += 1
-    });
-    console.log('targetTodoId', targetTodoId);
-    console.log( '.index', todos.find(item=>item.id === targetTodoId).index);
-    console.log(todos);
+    const newTodos = [...todos].map((item, i, arr) => {
+      if(item.id === id){
+         item.index--
+         if(arr[i-1]) {
+          arr[i-1].index++
+         }
+      }
+      return item
+    })
+    setTodos(newTodos);
+    handleSort();
   }
 
-  const decreaseIndex = ({target}) => {}
+  const decreaseIndex = (id) => {
+    const newTodos = [...todos].map((item, i, arr) => {
+      if(item.id === id){
+         item.index++
+         if(arr[i+1]) {
+          arr[i+1].index--
+         }
+      }
+      return item
+    })
+    setTodos(newTodos)
+    handleSort();
+  }
 
-  const addSublist = ({target}) => {
-    const targetTodoId = +target.closest('.todo-list__item').id;
-    console.log(targetTodoId)
-    const hauntedTodo = todos.find(item=> item.id === targetTodoId) 
-    console.log(hauntedTodo)
-    setTodos(() => {
-     // 
-      
-    });
+  const addSublist = (id) => {
+    let targetTodo = todos.find(item=> item.id === id) 
+    
+    targetTodo = {
+      ...targetTodo,
+      sublist: {},
+    }
+    console.log(targetTodo);
+
+    let newTodos = [...todos].map(item=> {
+      if(item.id === id) {
+        item.sublist = []
+      }
+      return item;
+    })
+    setTodos(newTodos)
   }
 
   return (
     <div className="todo-list">
-      <ul>
-        {todos.map((todo) => (
-          <li 
-            key={todo.id}
-            id={todo.id}
-            className="todo-list__item"
-          >
-          <p className="todo-list__todo">{todo.title}</p>
-          <button
-            onClick={increaseIndex}
-          >up</button>
-          <button
-
-          >down</button>
-          <button 
-            type="submit"
-            onClick={addSublist}
-          >Add Sublist</button>
-          <button 
-            onClick={removeListHandler}
-          >
-            Remove
-          </button>
-          <button>Remove sublist</button>
-          </li>
-        ))}
-        <input
-            type="text"
-            placeholder="type a todo here..."
-            className="todo-list__input"
-            value={todoTitle}
-            onChange={changeHandler}
-          ></input>
-          <button 
-            type="submit"
-            onClick={addTodoHandler}
-          >
-            Add
-          </button>
-          
-      </ul>
+      <main>
+         <Todo
+            todosProps={todos}
+           // todo={todo}
+            //i={i}
+            increaseIndex={increaseIndex}
+            decreaseIndex={decreaseIndex}
+            addSublist={addSublist}
+            removeListHandler={removeListHandler}
+            todoTitle={todoTitle}
+            changeHandler={changeHandler}
+            addTodoHandler={addTodoHandler}
+          />
+      </main>
+      <input
+        type="text"
+        placeholder="type a todo here..."
+        className="todo-list__input"
+        value={todoTitle}
+        onChange={changeHandler}
+      ></input>
+      <button 
+        type="submit"
+        onClick={addTodoHandler}
+      >
+        Add
+      </button>
     </div>
   );
 }
--- my-app/src/components/Todo/Todo.jsx
+++ my-app/src/components/Todo/Todo.jsx
@@ -1,24 +1,86 @@
 import React, {useState} from 'react'
+import classNames from 'classnames'
+export const Todo = ({todosProps, increaseIndex,
+  decreaseIndex, addSublist, removeListHandler}) => {
 
-export const Todo = ({todo, liftTodoHandler}) => {
-  /*const [todoTitle, setTodoTitle] = useState('');
+  const [todos, setTodos] = useState(todosProps);
+  const [todoTitle, setTodoTitle] = useState('');
+
+
   const changeHandler = ({target}) => {
     setTodoTitle(target.value)
   }
   
-  const addTodoHandler = ({target}) => {
-   // console.log(target.closest('.todo-list__item').children[1].value)
+  const addTodoHandler = () => {
     const newTodo = {
       id: +(new Date()),
-      title: target.closest('.todo-list__item').children[1].value
+      title: todoTitle,
+      index: todos.length + 1,
     }
-    liftTodoHandler(newTodo);
+    setTodos(currentState => ([
+      ...currentState,
+      newTodo,
+    ]))
     setTodoTitle('');
-  }*/
+  }
+  console.log(todos);
   return (
     <>
-      {/*<p>{todo}</p>*/}
-      
+    {todos?.map((todo, i) => (
+          <article 
+            key={todo.id}
+            className="todo-list__item"
+          >
+         <p className="todo-list__todo">{todo.title}</p>
+          <button
+            className={classNames('todo-list__button-up', {'todo-list__button-up--hidden': i === 0})}
+            onClick={() => increaseIndex(todo.id)}
+          > up </button>
+          <button
+            onClick={() => decreaseIndex(todo.id)}
+            className={classNames('todo-list__button-down', {'todo-list__button-down--hidden': i === todos.length - 1})}
+          > down </button>
+          <button 
+            type="submit"
+            onClick={()=>{addSublist(todo.id)}}
+          > Add Sublist </button>
+          <button 
+            onClick={()=>removeListHandler(todo.id)}
+          >
+            Remove
+          </button>
+          <button>Remove sublist</button>
+          
+          {todo.sublist /*&& todo.sublist.length */
+            ? <div><main><article>
+              <Todo 
+                todo={todo.sublist}
+               // i={i}
+                increaseIndex={increaseIndex}
+                decreaseIndex={decreaseIndex}
+                addSublist={addSublist}
+                removeListHandler={removeListHandler}
+            /> 
+            </article>
+            </main>
+            <input
+                type="text"
+                placeholder="type a todo here..."
+                className="todo-list__input"
+                value={todoTitle}
+                onChange={changeHandler}
+              ></input>
+              <button 
+                type="submit"
+                onClick={addTodoHandler}
+              >
+                Add
+              </button>
+            </div>
+            : null
+          }
+          </article>
+        ))}
     </>
   )
 }
