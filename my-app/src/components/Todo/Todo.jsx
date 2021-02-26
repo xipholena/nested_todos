ff8f95bed630f63efa466155f731fe61e054f572
Your Name <mogolgogol.90@gmail.com>
2021-02-26 10:46:58 +0200

    25 feb

--- my-app/src/components/Todo/Todo.jsx
+++ my-app/src/components/Todo/Todo.jsx
@@ -1,29 +1,86 @@
+
 import React, {useState} from 'react'
 import classNames from 'classnames'
-export const Todo = ({todosProps, increaseIndex,
-  decreaseIndex, addSublist, removeListHandler}) => {
 
-  const [todos, setTodos] = useState(todosProps);
+export const Todo = ({todos, setTodos}) => {
+  console.log(todos);
+  function useForceUpdate(){
+    const [value, setValue] = useState(0); 
+    return () => setValue(value => value + 1); 
+  }
+  const forceUpdate = useForceUpdate();
+
+  const [localTodos, setLocalTodos] = useState(todos);
+
   const [todoTitle, setTodoTitle] = useState('');
-
 
   const changeHandler = ({target}) => {
     setTodoTitle(target.value)
   }
   
   const addTodoHandler = () => {
+    
     const newTodo = {
       id: +(new Date()),
-      title: todoTitle,
+      title: todoTitle, 
       index: todos.length + 1,
     }
-    setTodos(currentState => ([
-      ...currentState,
-      newTodo,
-    ]))
+    todos.push(newTodo);
     setTodoTitle('');
   }
-  console.log(todos);
+
+  const removeListHandler = (id, subId) => {
+    setLocalTodos(localTodos=> localTodos.filter(item=> item.id !== id));
+  }
+
+  const handleSort = () => {
+    setTodos(todos=> todos.sort((a, b) => a.index - b.index))
+    console.log('ola')
+  }
+
+  const increaseIndex = (id) => {
+    
+    const newTodos = [...todos].map((item, i, arr) => {
+      if(item.id === id){ 
+         item.index-- 
+         if(arr[i-1]) {
+          arr[i-1].index++
+         }
+      }
+      return item
+    })
+    handleSort();
+    forceUpdate();
+  }
+
+  const decreaseIndex = (id) => {
+    const newTodos = [...todos].map((item, i, arr) => {
+      if(item.id === id){
+         item.index++
+         if(arr[i+1]) {
+          arr[i+1].index--
+         }
+      }
+      return item
+    })
+    handleSort();
+    forceUpdate();
+  }
+
+  const addSublist = (id) => {
+   
+    const newTodos = [...todos].map(item=> {
+      if(item.id === id) {
+        item.sublist = [{sublist: [], sublistId: +(new Date()), id: +(new Date())*2, index: 0}];
+        item.sublistId =  +(new Date());
+        
+      }
+      return item
+    })
+    
+      forceUpdate();
+  }
+
   return (
     <>
     {todos?.map((todo, i) => (
@@ -31,7 +88,7 @@ export const Todo = ({todosProps, increaseIndex,
             key={todo.id}
             className="todo-list__item"
           >
-         <p className="todo-list__todo">{todo.title}</p>
+          <p className="todo-list__todo">{todo.title}</p>
           <button
             className={classNames('todo-list__button-up', {'todo-list__button-up--hidden': i === 0})}
             onClick={() => increaseIndex(todo.id)}
@@ -43,44 +100,42 @@ export const Todo = ({todosProps, increaseIndex,
           <button 
             type="submit"
             onClick={()=>{addSublist(todo.id)}}
+            
           > Add Sublist </button>
           <button 
-            onClick={()=>removeListHandler(todo.id)}
+            onClick={()=>removeListHandler(todo.id, todo.sublistId)}
           >
             Remove
           </button>
           <button>Remove sublist</button>
-          
-          {todo.sublist /*&& todo.sublist.length */
-            ? <div><main><article>
+
+          {todo.sublist && todo.sublist.length
+            ? <div> 
               <Todo 
-                todo={todo.sublist}
-               // i={i}
-                increaseIndex={increaseIndex}
-                decreaseIndex={decreaseIndex}
-                addSublist={addSublist}
-                removeListHandler={removeListHandler}
+                todos={todo.sublist}
+                setTodos={setTodos}
             /> 
-            </article>
-            </main>
-            <input
-                type="text"
-                placeholder="type a todo here..."
-                className="todo-list__input"
-                value={todoTitle}
-                onChange={changeHandler}
-              ></input>
-              <button 
-                type="submit"
-                onClick={addTodoHandler}
-              >
-                Add
-              </button>
             </div>
             : null
           }
+
           </article>
+         
         ))}
+      <input
+        type="text"
+        placeholder="type a todo here..."
+        className="todo-list__input"
+        value={todoTitle}
+        onChange={changeHandler}
+      ></input>
+      <button 
+        type="submit"
+        onClick={addTodoHandler}
+      >
+        Add
+      </button>
     </>
   )
 }
+
